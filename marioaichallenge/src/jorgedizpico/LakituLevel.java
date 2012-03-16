70af45a483698834f35e11c81d2869367f2ab2d9
jorge <jorge@moulinsart>
2012-03-16 18:18:35 +0100

    LakituLevel is the one we are working on. UrLevel stores the unholy child of CustomizedLevel and RandomLevel, for reference.

---
+++ marioaichallenge/src/jorgedizpico/LakituLevel.java
@@ -0,0 +1,21 @@
+package jorgedizpico;
+
+import java.util.Random;
+
+import dk.itu.mario.MarioInterface.Constraints;
+import dk.itu.mario.MarioInterface.GamePlay;
+import dk.itu.mario.MarioInterface.LevelInterface;
+import dk.itu.mario.engine.sprites.Enemy;
+import dk.itu.mario.engine.sprites.SpriteTemplate;
+import dk.itu.mario.level.Level;
+
+public class LakituLevel extends Level implements LevelInterface {
+
+    private GamePlay playerM;
+
+    public LakituLevel(int width, int height, long seed, int difficulty,
+                           int type, GamePlay playerMetrics) {
+        super(width, height);
+        this.playerM = playerMetrics;
+    }
+}
--- marioaichallenge/src/jorgedizpico/LakituLevelGenerator.java
+++ marioaichallenge/src/jorgedizpico/LakituLevelGenerator.java
@@ -10,10 +10,17 @@ public class LakituLevelGenerator implements LevelGenerator {
 
 	@Override
 	public LevelInterface generateLevel(GamePlay playerMetrics) {
-		int difficulty = 5;
-		int type = 3;
-		return new LakituLevel(320, 15, new Random().nextLong(), difficulty, type, playerMetrics);
-		//LevelInterface level = new CustomizedLevel(320,15,new Random().nextLong(),1,1,playerMetrics);
+		int[] paramArray = process_metrics(playerMetrics);
+		int playerSkill = paramArray[0] + 5;
+		int[] mapWave = draw_wave(playerSkill);
+		ground_pass(mapWave, paramArray);
+		// hill_pass(mapWave, paramArray);
+		// pipe_pass(mapWave, paramArray);
+		// enemy_pass(mapWave, paramArray);
+		// block_pass(mapWave, paramArray);
+		// coin_pass(mapWave, paramArray);
+		int type = new Random().nextInt(3); //0: over, 1: under, 2: castle
+		return new LakituLevel(320, 15, new Random().nextLong(), playerSkill, type, playerMetrics);
 	}
 
 	@Override
@@ -21,5 +28,20 @@ public class LakituLevelGenerator implements LevelGenerator {
 		// TODO Auto-generated method stub
 		return null;
 	}
+	
+	private int[] process_metrics(GamePlay playerMetrics) {
+		// takes the metrics, produces an array of parameters we'll need
+		// possible ANN?
+		return null;
+	}
 
+	private int[] draw_wave(int playerSkill) {
+		// takes skill and builds a tension wave
+		return null;
+	}
+	
+	private void ground_pass(int[] mapWave, int[] paramArray) {
+		// apply probabilities from the paramArray to fill the mapWave
+	}
+	
 }
